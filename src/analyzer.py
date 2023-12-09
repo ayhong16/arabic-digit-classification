Fix EM covariance input and output

src/analyzer.py
@@ -5,7 +5,7 @@ import numpy as np
 from sklearn.neighbors import KernelDensity
 
 from dataparser import DataParser
-from em import em, plot_em_contours, em_component_gmm_helper
+from em import em, plot_em_contours, em_component_gmm_helper, fix_em_cov_input
 from kmeans import k_means, plot_kmeans_contours, kmeans_component_gmm_helper
 from util import compute_likelihood, get_comp_covariance, classify_utterance
 
@@ -17,7 +17,7 @@ class Analyzer:
         self.train_df = parser.train_df
         self.test_df = parser.test_df
         self.mfccs = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-        self.cov = {"tied": False, "cov_type": "full"}
+        self.cov = {"tied": True, "cov_type": "spherical"}
         self.use_kmeans = False
         self.phoneme_map = {
             0: 4,
@@ -91,11 +91,13 @@ class Analyzer:
         # fig.suptitle("K-Means GMMs for Various 2D Plots for Digit " + str(token))
         # plt.tight_layout()
 
-    def plot_em_gmm(self, token, comparisons, n_components, cov_type, ax):
+    def plot_em_gmm(self, token, comparisons, ax=None):
         data = self.get_all_training_utterances(token)
         # fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(12, 6))
         colors = ["c", "r", "g", "m", 'y']
-        cluster_info = em(n_components, data, cov_type)
+        cov_in = fix_em_cov_input(self.cov["tied"], self.cov["cov_type"])
+        n_components = self.phoneme_map[token]
+        cluster_info = em(self.mfccs, n_components, data, cov_in)
         for i in range(len(comparisons)):
             first_comp = comparisons[i][0]
             second_comp = comparisons[i][1]
@@ -104,14 +106,22 @@ class Analyzer:
                 c = colors[color_ind]
                 coords = np.column_stack(((cluster_info[key]["data"][:, second_comp - 1],
                                            cluster_info[key]["data"][:, first_comp - 1])))
-                ax.scatter(coords[:, 0], coords[:, 1], s=.1, c=c, alpha=.8)
+                if ax is not None:
+                    ax.scatter(coords[:, 0], coords[:, 1], s=.1, c=c, alpha=.8)
+                else:
+                    plt.scatter(coords[:, 0], coords[:, 1], s=.1, c=c, alpha=.8)
                 cov = get_comp_covariance(second_comp - 1, first_comp - 1, cluster_info[key]["cov"])
-                plot_em_contours([key[second_comp - 1], key[first_comp - 1]], cov, c, ax, coords)
+                plot_em_contours([key[second_comp - 1], key[first_comp - 1]], cov, c, coords, ax)
                 color_ind += 1
             # ax.set_title("MFCC " + str(first_comp) + "(y) vs. MFCC" + str(second_comp) + "(x)")
-            ax.set_title("EM Clustering")
-            ax.set_xlabel("MFCC " + str(second_comp))
-            ax.set_ylabel("MFCC " + str(first_comp))
+            if ax is not None:
+                ax.set_title(f"Digit {token} {"Tied" if self.cov['tied'] else "Distinct"} {self.cov["cov_type"]} Covariance EM Clustering")
+                ax.set_xlabel("MFCC " + str(second_comp))
+                ax.set_ylabel("MFCC " + str(first_comp))
+            else:
+                plt.title(f"Digit {token} {"Tied" if self.cov['tied'] else "Distinct"} {self.cov["cov_type"]} Covariance EM Clustering")
+                plt.xlabel("MFCC " + str(second_comp))
+                plt.ylabel("MFCC " + str(first_comp))
         # fig.suptitle("EM GMMs for Various 2D Plots for Digit " + str(token))
         # plt.tight_layout()
 
@@ -138,7 +148,8 @@ class Analyzer:
             cluster_info = k_means(n_clusters, data)
             components = kmeans_component_gmm_helper(cluster_info, data, covariance_type, tied)
         else:
-            cluster_info = em(n_clusters, data, covariance_type)
+            cov_in = fix_em_cov_input(tied, covariance_type)
+            cluster_info = em(self.mfccs, n_clusters, data, cov_type=cov_in)
             components = em_component_gmm_helper(cluster_info)
         cov_tied = "tied " if tied else "distinct "
         ret = {
@@ -184,6 +195,12 @@ class Analyzer:
 
     def compute_confusion_matrix(self, cov_type=None, use_kmeans=None, tied=None):
         confusion = np.zeros((10, 10))
+        if cov_type is None:
+            cov_type = self.cov["cov_type"]
+        if use_kmeans is None:
+            use_kmeans = self.use_kmeans
+        if tied is None:
+            tied = self.cov["tied"]
         gmms = [self.estimate_gmm_params(digit, cov_type, use_kmeans, tied) for digit in range(10)]
         for digit in range(10):
             classification = self.classify_all_utterances(digit, gmms)
@@ -206,3 +223,4 @@ class Analyzer:
         data = metadata['MFCCs']
         x = [i for i in range(len(data))]
         return x, np.array(data)
+

src/em.py
@@ -1,18 +1,17 @@
 import numpy as np
+from matplotlib import pyplot as plt
 from scipy.stats import multivariate_normal
 
 from GaussianMixture import GaussianMixture
 
 
-def em(n_components, data, cov_type):
+def em(mfccs, n_components, data, cov_type):
     gmm = GaussianMixture(n_components=n_components, covariance_type=cov_type, random_state=5)
     gmm.fit(data)
     labels = gmm.predict(data)
     means = gmm.means_
-    covars = gmm.covariances_
+    covars = fix_em_cov_output(mfccs, gmm.covariances_, cov_type, n_components)
     weights = gmm.weights_
-    if len(covars) == 1:
-        covars = np.tile(covars, n_components)
     cluster_info = {}
     for cluster_label in range(n_components):
         cluster_data = data[labels == cluster_label]
@@ -24,7 +23,33 @@ def em(n_components, data, cov_type):
     return cluster_info
 
 
-def plot_em_contours(mean, cov, color, ax, coords):
+def _fix_em_cov_output(self, covariance, covariance_type, covariance_tied, k):
+    mfcc_length = len([x for x in self.hyperparams["mfcc_indexes"] if x >= 0])
+    if covariance_type == "full":
+        if covariance_tied:
+            ret = np.asarray([covariance] * k)
+        else:
+            ret = covariance
+    elif covariance_type == "diagonal" or covariance_type == "diag":
+        if covariance_tied:
+            ret = np.asarray([np.diag(cov) for cov in covariance])
+        else:
+            ret = np.asarray([np.diag(cov) for cov in covariance])
+    elif covariance_type == "spherical":
+        if covariance_tied:
+            ret = np.asarray([np.diag([covariance] * mfcc_length)] * k)
+        else:
+            ret = np.asarray([np.diag([cov] * mfcc_length) for cov in covariance])
+    else:
+        ret = ValueError("Invalid covariance type: " + covariance_type)
+    assert len(ret.shape) == 3
+    assert ret.shape[0] == k
+    assert ret.shape[1] == mfcc_length
+    assert ret.shape[2] == mfcc_length
+    return ret
+
+
+def plot_em_contours(mean, cov, color, coords, ax=None):
     maxx_diff = max([abs(x[0] - mean[0]) for x in coords])
     maxy_diff = max([abs(x[1] - mean[1]) for x in coords])
     x = np.linspace(mean[0] - maxx_diff, mean[0] + maxx_diff, 1000)
@@ -34,7 +59,10 @@ def plot_em_contours(mean, cov, color, ax, coords):
     pos = np.empty(X.shape + (2,))
     pos[:, :, 0] = X
     pos[:, :, 1] = Y
-    ax.contour(X, Y, z.pdf(pos), colors=color, alpha=.6)
+    if ax is None:
+        plt.contour(X, Y, z.pdf(pos), colors=color, alpha=.6)
+    else:
+        ax.contour(X, Y, z.pdf(pos), colors=color, alpha=.6)
 
 
 def em_component_gmm_helper(cluster_info):
@@ -46,3 +74,36 @@ def em_component_gmm_helper(cluster_info):
             "covariance": cluster_info[key]["cov"]
         })
     return components
+
+
+def fix_em_cov_input(tied, cov_type):
+    if tied:
+        if cov_type == "full":
+            return "tied"
+        if cov_type == "diag":
+            return "tied_diag"
+        if cov_type == "spherical":
+            return "tied_spherical"
+    else:
+        return cov_type
+
+
+def fix_em_cov_output(mfccs, covariance, covariance_type, n_components):
+    mfcc_length = len(mfccs)
+    if covariance_type == "full":
+        ret = covariance
+    elif covariance_type == "tied":
+        ret = np.asarray([covariance] * n_components)
+    elif covariance_type == "tied_diag" or covariance_type == "diag":
+        ret = np.asarray([np.diag(cov) for cov in covariance])
+    elif covariance_type == "tied_spherical":
+        ret = np.asarray([np.diag([cov] * mfcc_length) for cov in covariance])
+    elif covariance_type == "spherical":
+        ret = np.asarray([np.diag([cov] * mfcc_length) for cov in covariance])
+    else:
+        ret = ValueError("Invalid covariance type: " + covariance_type)
+    assert len(ret.shape) == 3
+    assert ret.shape[0] == n_components
+    assert ret.shape[1] == mfcc_length
+    assert ret.shape[2] == mfcc_length
+    return ret

src/plotter.py
@@ -3,7 +3,7 @@ from pprint import pprint
 import numpy as np
 from matplotlib import pyplot as plt
 from analyzer import Analyzer
-from confusionplotter import compute_accuracy, compute_precision
+from confusionplotter import compute_accuracy, compute_precision, plot_confusion_matrix
 
 
 def kmeans_label(kmeans):
@@ -55,9 +55,11 @@ def plot_likelihoods():
 
 def plot_em_contours():
     analyzer = Analyzer()
-    comps = [(1, 2), (2, 3), (1, 3)]
-    for d in range(10):
-        analyzer.plot_em_gmm(d, comps, analyzer.phoneme_map[d], "full")
+    comps = [(1, 2)]
+    analyzer.plot_em_gmm(0, comps)
+    # comps = [(1, 2), (2, 3), (1, 3)]
+    # for d in range(10):
+    #     analyzer.plot_em_gmm(d, comps, analyzer.phoneme_map[d])
     plt.show()
 
 
@@ -66,7 +68,7 @@ def plot_em_kmeans_contours():
     comps = [(1, 2)]
     for d in range(10):
         fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(12, 6))
-        analyzer.plot_em_gmm(d, comps, analyzer.phoneme_map[d], "full", axes[0])
+        analyzer.plot_em_gmm(d, comps, analyzer.phoneme_map[d], axes[0])
         analyzer.plot_kmeans_gmm(d, comps, analyzer.phoneme_map[d], axes[1])
         plt.suptitle(f"Digit {d} Comparison of EM and K-Means")
         plt.tight_layout()
@@ -117,18 +119,18 @@ def create_cluster_accuracy_map(analyzer):
 def plot_cov_performance():
     analyzer = Analyzer()
     # accuracy_map, precision_map = create_cov_performance_map(analyzer)
-    accuracy_map = {'diag': {'em': {'distinct': 85.77, 'tied': 85.77},
+    accuracy_map = {'diag': {'em': {'distinct': 85.77, 'tied': 82.46},
                              'kmeans': {'distinct': 52.86000000000001, 'tied': 74.07999999999998}},
-                    'full': {'em': {'distinct': 90.23, 'tied': 90.23},
+                    'full': {'em': {'distinct': 90.23, 'tied': 88.36},
                              'kmeans': {'distinct': 87.53999999999999, 'tied': 87.91}},
-                    'spherical': {'em': {'distinct': 73.27, 'tied': 73.27},
+                    'spherical': {'em': {'distinct': 73.27, 'tied': 74.09},
                                   'kmeans': {'distinct': 52.86000000000001, 'tied': 74.07999999999998}}}
-    precision_map = {'diag': {'em': {'distinct': .8765173785778823, 'tied': .8765173785778823},
-                              'kmeans': {'distinct': .6456228540617242, 'tied': .7766820352299685}},
-                     'full': {'em': {'distinct': .9050832188255168, 'tied': .9050832188255168},
-                              'kmeans': {'distinct': .8887162531107379, 'tied': .8878313045295714}},
-                     'spherical': {'em': {'distinct': .7675264417271109, 'tied': .7675264417271109},
-                                   'kmeans': {'distinct': .6456228540617242, 'tied': .7766820352299685}}}
+    precision_map = {'diag': {'em': {'distinct': 0.8765173785778824, 'tied': 0.8443208527942513},
+                              'kmeans': {'distinct': 0.6456228540617243, 'tied': 0.7766820352299685}},
+                     'full': {'em': {'distinct': 0.9050832188255169, 'tied': 0.8887451716946844},
+                              'kmeans': {'distinct': 0.8887162531107379, 'tied': 0.8878313045295714}},
+                     'spherical': {'em': {'distinct': 0.7675264417271108, 'tied': 0.7756454945695905},
+                                   'kmeans': {'distinct': 0.6456228540617243, 'tied': 0.7766820352299685}}}
     pprint(accuracy_map)
     pprint(precision_map)
 
@@ -209,8 +211,33 @@ def plot_cluster_accuracy():
     plt.show()
 
 
+def plot_tied_distinct_contours():
+    analyzer = Analyzer()
+    comps = [(1, 2)]
+    for d in range(10):
+        fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(12, 6))
+        analyzer.cov["tied"] = True
+        analyzer.plot_em_gmm(d, comps, axes[0])
+        analyzer.cov["tied"] = False
+        analyzer.plot_em_gmm(d, comps, axes[1], )
+        plt.suptitle(f"Digit {d} Comparison of Tied and Distinct Covariance Matrices")
+        # plt.savefig(f"../plots/tied_vs_distinct/digit_{d}.png")
+        plt.tight_layout()
+    plt.show()
+
+
 def plot_greedy_mfcc():
     pass
+
+
+def test():
+    analyzer = Analyzer()
+    # analyzer.cov["tied"] = False
+    # analyzer.cov["cov_type"] = "full"
+    confusion = analyzer.compute_confusion_matrix()
+    print(f'Accuracy: {np.mean(compute_accuracy(confusion))}')
+    print(f'Precision: {np.mean(compute_precision(confusion))}')
+    plot_confusion_matrix(confusion)
 
 
 if __name__ == '__main__':
@@ -218,8 +245,10 @@ if __name__ == '__main__':
     # plot_pairwise_scatter()
     # plot_kmeans_contours()
     # plot_em_kmeans_contours()
+    # plot_tied_distinct_contours()
     # plot_likelihoods()
     # plot_em_contours()
-    # plot_cov_performance()
-    plot_cluster_accuracy()
+    plot_cov_performance()
+    # plot_cluster_accuracy()
     # plot_greedy_mfcc()
+    # test()